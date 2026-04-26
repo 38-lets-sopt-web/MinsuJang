2b347433f7199bd10352d726cb8d1e6cedbd96d0
Tnalxmsk <alstnwkd990@naver.com>
2026-04-26 15:57:59 +0900

    init: setup prettier

--- week3/homework/eslint.config.js
+++ week3/homework/eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -15,8 +17,15 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      'prettier/prettier': 'error',
+    },
   },
-])
+  eslintConfigPrettier,
+]);
