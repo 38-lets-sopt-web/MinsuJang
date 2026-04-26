d31c7d0a5e8a32f97abf2df4cf574e03deb11773
Tnalxmsk <alstnwkd990@naver.com>
2026-04-26 16:15:06 +0900

    chore: 절대경로 설정

--- week3/homework/vite.config.ts
+++ week3/homework/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+    },
+  },
+  plugins: [vanillaExtractPlugin(), react(), babel({ presets: [reactCompilerPreset()] })],
 });
