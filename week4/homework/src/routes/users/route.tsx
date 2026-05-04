43c907eb567e253203b5f6195bd80bbbb7eaac56
Tnalxmsk <alstnwkd990@naver.com>
2026-05-05 03:40:53 +0900

    feat: add protected users route guard

--- week4/homework/src/routes/users/route.tsx
+++ week4/homework/src/routes/users/route.tsx
@@ -1,6 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
+import { getStoredUserId } from '@apis/core';
 import { UsersLayout } from './-UsersLayout';
 
 export const Route = createFileRoute('/users')({
+  beforeLoad: () => {
+    const userId = getStoredUserId();
+
+    if (!userId) {
+      throw redirect({ to: '/signin' });
+    }
+
+    return { userId };
+  },
   component: UsersLayout,
 });
